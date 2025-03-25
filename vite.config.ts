526fc2bef80b801ada5019607158307b0a1bfba5
TArch64 <tarasturchenko1551@gmail.com>
2025-03-25 23:22:14 +0200

    setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,34 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import postcssNesting from 'postcss-nesting';
 
-// https://vite.dev/config/
 export default defineConfig({
+  clearScreen: false,
+
   plugins: [
     vue(),
     vueJsx(),
     vueDevTools(),
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+
+  build: {
+    cssMinify: 'lightningcss'
+  },
+
+  css: {
+    transformer: 'lightningcss',
+
+    postcss: {
+      plugins: [
+        postcssNesting()
+      ]
+    }
+  }
 })
