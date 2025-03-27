de150cb693386cc49db430f81c6819ae8b079620
TArch64 <tarasturchenko1551@gmail.com>
2025-03-27 22:02:56 +0200

    add welcome screen

--- src/modules/welcome/Route.vue
+++ src/modules/welcome/Route.vue
@@ -1,3 +1,16 @@
 <template>
-  <p>welcome</p>
+  <WelcomeLayout>
+    <h1 class="welcome-header">Polimane</h1>
+    <p>Твій помічник у створенні схем для бісеру</p>
+  </WelcomeLayout>
 </template>
+
+<script setup lang="ts">
+import { WelcomeLayout } from './components';
+</script>
+
+<style scoped>
+.welcome-header {
+  margin-bottom: 8px;
+}
+</style>
---
+++ src/modules/welcome/components/WelcomeLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="welcome-layout">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Slot } from 'vue';
+
+defineSlots<{
+  default: Slot;
+}>();
+</script>
+
+<style scoped>
+@layer page {
+  .welcome-layout {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>
