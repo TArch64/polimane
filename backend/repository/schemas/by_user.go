3f30f44a60ab79ebf5914e8c4acfd7eee117b113
TArch64 <tarasturchenko1551@gmail.com>
2025-04-17 20:47:23 +0300

    remove extra hash

--- backend/repository/schemas/by_user.go
+++ backend/repository/schemas/by_user.go
@@ -14,7 +14,7 @@ func ByUser(ctx context.Context, user *model.User, attributes []string) ([]*mode
 
 	query := awsdynamodb.Table().
 		Get("PK", user.ID).
-		Range("SK", dynamo.BeginsWith, model.SKSchema+"#")
+		Range("SK", dynamo.BeginsWith, model.SKSchema)
 
 	if attributes != nil && len(attributes) > 0 {
 		attributes = append([]string{"PK", "SK"}, attributes...)
