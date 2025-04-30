08621222e3ecf30b275da6de19566ac4d401f043
TArch64 <tarasturchenko1551@gmail.com>
2025-04-30 20:18:27 +0300

    add render rows

--- frontend/src/modules/schemas/editor/components/content/pattern/CanvasPattern.vue
+++ frontend/src/modules/schemas/editor/components/content/pattern/CanvasPattern.vue
@@ -6,19 +6,24 @@
   >
     <KonvaRect ref="borderRef" :config="borderConfig" />
     <CanvasPatternLabel :pattern />
-    <CanvasPatternEmpty v-if="!pattern.content.length" />
+
+    <KonvaGroup ref="contentGroupRef">
+      <CanvasPatternContent :pattern v-if="pattern.content.length" />
+      <CanvasPatternEmpty v-else />
+    </KonvaGroup>
   </KonvaGroup>
 </template>
 
 <script setup lang="ts">
 import Konva from 'konva';
-import { computed, type ComputedRef, ref } from 'vue';
-import type { KonvaRect } from 'vue-konva';
+import { computed, type ComputedRef, onMounted, reactive, ref } from 'vue';
+import type { KonvaGroup, KonvaRect } from 'vue-konva';
 import type { ISchemaPattern } from '@/models';
 import { useNodeHover, useNodeTween } from '@/modules/schemas/editor/composables';
 import { useModal } from '@/components/modal';
 import { getPatternAddRowModal } from '../../modals';
 import CanvasPatternLabel from './CanvasPatternLabel.vue';
+import CanvasPatternContent from './CanvasPatternContent.vue';
 import CanvasPatternEmpty from './CanvasPatternEmpty.vue';
 
 const props = defineProps<{
@@ -26,25 +31,26 @@ const props = defineProps<{
 }>();
 
 const addModal = useModal(getPatternAddRowModal(props.pattern));
-const openAddRowModal = () => !props.pattern.content.length && addModal.open({ pattern: props.pattern });
+const openAddRowModal = () => addModal.open({ pattern: props.pattern });
 
 const borderRef = ref<InstanceType<KonvaRect> | null>(null);
 const borderNode: ComputedRef<Konva.Rect | null> = computed(() => borderRef.value?.getNode());
+
+const contentGroupRef = ref<InstanceType<KonvaGroup>>(null!);
+const contentGroupNode: ComputedRef<Konva.Group> = computed(() => contentGroupRef.value.getNode());
 
 const config: Partial<Konva.GroupConfig> = {
   id: props.pattern.id,
 };
 
-const borderConfig: Partial<Konva.RectConfig> = {
+const borderConfig: Partial<Konva.RectConfig> = reactive({
   x: 1,
   y: 1,
-  width: 1000,
-  height: 100,
   stroke: 'rgba(0, 0, 0, 0.1)',
   strokeWidth: 1,
   cornerRadius: 8,
   dash: [10, 5],
-};
+});
 
 const borderHover = useNodeHover();
 
@@ -53,4 +59,10 @@ useNodeTween(borderNode, () => borderHover.isHovered, (isHovered) => ({
   stroke: isHovered ? 'rgba(0, 0, 0, 0.5)' : borderConfig.stroke!,
   easing: Konva.Easings.EaseOut,
 }));
+
+onMounted(() => {
+  const contentRect = contentGroupNode.value.getClientRect();
+  borderConfig.width = Math.max((contentRect.width + 24), 1000);
+  borderConfig.height = Math.max((contentRect.height + 16), 100);
+});
 </script>
---
+++ frontend/src/modules/schemas/editor/components/content/row/CanvasRow.vue
@@ -0,0 +1,18 @@
+<template>
+  <KonvaRect :config />
+</template>
+
+<script setup lang="ts">
+import Konva from 'konva';
+import type { ISchemaRow } from '@/models';
+
+defineProps<{
+  row: ISchemaRow;
+}>();
+
+const config: Partial<Konva.RectConfig> = {
+  width: 800,
+  height: 20,
+  fill: 'rgba(0, 0, 0, 0.2)',
+};
+</script>
