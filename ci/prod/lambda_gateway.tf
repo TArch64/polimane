f3f043e038ec1718b9f2a6ddb59fc387eb09d476
TArch64 <tarasturchenko1551@gmail.com>
2025-06-08 20:46:24 +0300

    fix gateway api

--- ci/prod/lambda_gateway.tf
+++ ci/prod/lambda_gateway.tf
@@ -6,7 +6,7 @@ resource "aws_api_gateway_rest_api" "lambda_api" {
 resource "aws_api_gateway_resource" "lambda_resource" {
   rest_api_id = aws_api_gateway_rest_api.lambda_api.id
   parent_id   = aws_api_gateway_rest_api.lambda_api.root_resource_id
-  path_part   = "api"
+  path_part = "{proxy+}"
 }
 
 resource "aws_api_gateway_method" "lambda_method" {
@@ -20,21 +20,33 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id = aws_api_gateway_rest_api.lambda_api.id
   resource_id = aws_api_gateway_resource.lambda_resource.id
   http_method = aws_api_gateway_method.lambda_method.http_method
-
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.lambda.invoke_arn
 }
 
 resource "aws_api_gateway_deployment" "lambda_deployment" {
-  depends_on = [aws_api_gateway_integration.lambda_integration,]
+  depends_on = [aws_api_gateway_integration.lambda_integration]
   rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+
+  triggers = {
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.lambda_resource.id,
+      aws_api_gateway_method.lambda_method.id,
+      aws_api_gateway_integration.lambda_integration.id,
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_api_gateway_stage" "lambda_stage" {
   deployment_id = aws_api_gateway_deployment.lambda_deployment.id
   rest_api_id   = aws_api_gateway_rest_api.lambda_api.id
   stage_name    = "prod"
+  tags = local.aws_common_tags
 }
 
 resource "aws_lambda_permission" "api_gateway_invoke" {
