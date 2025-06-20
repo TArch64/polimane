d6b3e30525de8cc1af4336ad82aaa209f1c07bdd
TArch64 <tarasturchenko1551@gmail.com>
2025-06-20 21:29:02 +0300

    add s3 private acl

--- ci/prod/s3.tf
+++ ci/prod/s3.tf
@@ -7,3 +7,18 @@ resource "aws_s3_bucket" "bucket" {
   bucket = local.app_name
   tags = local.aws_common_tags
 }
+
+resource "aws_s3_bucket_ownership_controls" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket" {
+  depends_on = [aws_s3_bucket_ownership_controls.bucket]
+
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
+}
