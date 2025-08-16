8e3d266ad077126243d6fc25a08309f3ce02bdca
TArch64 <tarasturchenko1551@gmail.com>
2025-08-16 12:51:06 +0300

    improve preformance

--- frontend/src/modules/schemas/editor/components/content/CanvasBead.ts
+++ frontend/src/modules/schemas/editor/components/content/CanvasBead.ts
@@ -3,7 +3,6 @@ import Konva from 'konva';
 import { getThemeVar } from '@/composables';
 import { SCREENSHOT_IGNORE } from '../../composables';
 import { BEAD_SIZE, type BeadPosition } from './useBeadsGrid';
-import { GroupRenderer } from './base';
 
 export interface ICanvasBeadProps {
   offset: BeadPosition;
@@ -13,28 +12,25 @@ export interface ICanvasBeadProps {
 export const CanvasBead: FunctionalComponent<ICanvasBeadProps> = (props) => {
   const KonvaRect = resolveComponent('KonvaRect');
 
-  const groupConfig: Partial<Konva.GroupConfig> = {
-    name: props.color ? undefined : SCREENSHOT_IGNORE,
+  const backgroundConfig: Partial<Konva.RectConfig> = {
     x: props.offset[0],
     y: props.offset[1],
-  };
-
-  const backgroundConfig: Partial<Konva.RectConfig> = {
     width: BEAD_SIZE,
     height: BEAD_SIZE,
   };
 
   const contentConfig: Partial<Konva.RectConfig> = {
-    x: 1,
-    y: 1,
+    name: props.color ? undefined : SCREENSHOT_IGNORE,
+    x: props.offset[0] + 1,
+    y: props.offset[1] + 1,
     width: BEAD_SIZE - 2,
     height: BEAD_SIZE - 2,
     cornerRadius: getThemeVar('--rounded-full'),
     fill: props.color ?? getThemeVar('--color-background-3'),
   };
 
-  return h(GroupRenderer, { config: groupConfig }, () => [
+  return [
     h(KonvaRect, { config: backgroundConfig }),
     h(KonvaRect, { config: contentConfig }),
-  ]);
+  ];
 };
--- frontend/src/modules/schemas/editor/components/content/base/stack/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as CanvasStackV } from './CanvasStackV.vue';
-export { default as CanvasStackH } from './CanvasStackH.vue';
