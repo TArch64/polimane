66dec31b69e47e6e9f803c79e20d507bb180053c
TArch64 <tarasturchenko1551@gmail.com>
2025-09-28 14:17:50 +0300

    improve security configuration

--- ci/prod/lambda_gateway.tf
+++ ci/prod/lambda_gateway.tf
@@ -19,7 +19,7 @@ resource "aws_apigatewayv2_api" "lambda_api" {
       "Cookie"
     ]
     allow_credentials = true
-    expose_headers = ["*"]
+    expose_headers = ["X-New-Access-Token", "X-New-Refresh-Token"]
   }
 }
 
