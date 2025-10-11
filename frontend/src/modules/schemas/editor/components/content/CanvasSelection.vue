fd0973129cd5a447bda59506fa3d10b51e0358eb
TArch64 <tarasturchenko1551@gmail.com>
2025-10-11 23:56:39 +0300

    remove secondary selection

--- frontend/src/modules/schemas/editor/components/content/CanvasSelection.vue
+++ frontend/src/modules/schemas/editor/components/content/CanvasSelection.vue
@@ -1,35 +1,23 @@
 <template>
-  <g>
-    <CanvasDefs>
-      <rect
-        :x
-        :y
-        :width
-        :height
-        :id="partialId"
-        rx="2"
-        ry="2"
-        fill="none"
-        stroke-width="1"
-        class="canvas-selection"
-      />
-    </CanvasDefs>
-
-    <use :href="partialHref" :stroke="emptyColor" />
-    <use :href="partialHref" :stroke="selectionColor" stroke-dasharray="4 3" />
-  </g>
+  <rect
+    :x
+    :y
+    :width
+    :height
+    rx="2"
+    ry="2"
+    fill="none"
+    stroke-width="1"
+    class="canvas-selection"
+    :stroke="selectionColor"
+    stroke-dasharray="4 3"
+  />
 </template>
 
 <script setup lang="ts">
-import { computed, useId } from 'vue';
+import { computed } from 'vue';
 import type { IBeadSelection } from '@editor/stores';
-import {
-  BEAD_SIZE,
-  type IBeadsGrid,
-  useBackgroundCanvasColor,
-  useSelectionColor,
-} from '@editor/composables';
-import CanvasDefs from './CanvasDefs.vue';
+import { BEAD_SIZE, type IBeadsGrid, useSelectionColor } from '@editor/composables';
 
 const props = defineProps<{
   selected: IBeadSelection;
@@ -41,15 +29,11 @@ const toOffset = computed(() => props.beadsGrid.resolveBeadOffset(props.selected
 
 const PADDING = 2;
 
-const partialId = `editorSelection-${useId()}`;
-const partialHref = `#${partialId}`;
-
 const x = computed(() => Math.min(fromOffset.value[0], toOffset.value[0]) - PADDING);
 const y = computed(() => Math.min(fromOffset.value[1], toOffset.value[1]) - PADDING);
 const width = computed(() => Math.abs(fromOffset.value[0] - toOffset.value[0]) + BEAD_SIZE + PADDING * 2);
 const height = computed(() => Math.abs(fromOffset.value[1] - toOffset.value[1]) + BEAD_SIZE + PADDING * 2);
 
-const emptyColor = useBackgroundCanvasColor();
 const selectionColor = useSelectionColor();
 </script>
 
