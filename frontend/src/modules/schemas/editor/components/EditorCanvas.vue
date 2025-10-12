c57a4d9d04f3711e35f223bc7461bca8d50ef560
TArch64 <tarasturchenko1551@gmail.com>
2025-10-13 00:49:45 +0300

    add moving selection

--- frontend/src/modules/schemas/editor/components/EditorCanvas.vue
+++ frontend/src/modules/schemas/editor/components/EditorCanvas.vue
@@ -4,6 +4,7 @@
       ref="canvasRef"
       xmlns="http://www.w3.org/2000/svg"
       preserveAspectRatio="xMidYMin slice"
+      class="canvas-editor"
       :width="wrapperRect.width"
       :height="wrapperRect.height"
       :viewBox
--- frontend/src/modules/schemas/editor/components/content/selection/CanvasSelection.vue
+++ frontend/src/modules/schemas/editor/components/content/selection/CanvasSelection.vue
@@ -5,33 +5,50 @@
       :y
       :width
       :height
+      :stroke="selectionColor"
       rx="2"
       ry="2"
       fill="none"
-      ref="selectionRef"
-      class="canvas-selection"
-      :stroke="selectionColor"
+      class="canvas-selection canvas-selection--original"
       stroke-dasharray="4 3"
+      ref="selectionRef"
+    />
+
+    <rect
+      :x="movingX"
+      :y="movingY"
+      :width="movingWidth"
+      :height="movingHeight"
+      :stroke="selectionColor"
+      rx="2"
+      ry="2"
+      fill="none"
+      class="canvas-selection canvas-selection--moving"
+      stroke-opacity="0.5"
+      v-if="isMoving"
     />
 
     <ForeignTeleport>
+      <div
+        class="canvas-selection__overlay"
+        v-if="isOverlayDisplaying"
+      />
+
       <SelectionArea :selectionRef v-if="selectionRef">
         <SelectionResizeHandle
           v-for="direction of DirectionList"
-          :key="direction"
           :direction
+          :key="direction"
           v-model:translation="translation[direction]"
           v-model:overlay="isOverlayDisplaying"
         />
       </SelectionArea>
-
-      <div class="canvas-selection__overlay" v-if="isOverlayDisplaying" />
     </ForeignTeleport>
   </g>
 </template>
 
 <script setup lang="ts">
-import { reactive, ref } from 'vue';
+import { computed, reactive, ref } from 'vue';
 import { type IBeadSelection, useCanvasStore } from '@editor/stores';
 import { BEAD_SIZE, type IBeadsGrid, useSelectionColor } from '@editor/composables';
 import { Direction, DirectionList } from '@/enums';
@@ -45,9 +62,9 @@ const props = defineProps<{
 }>();
 
 const canvasStore = useCanvasStore();
+const selectionColor = useSelectionColor();
 
 const selectionRef = ref<SVGElement>(null!);
-
 const isOverlayDisplaying = ref(false);
 
 const translation = reactive<Record<Direction, number>>({
@@ -55,6 +72,10 @@ const translation = reactive<Record<Direction, number>>({
   [Direction.BOTTOM]: 0,
   [Direction.LEFT]: 0,
   [Direction.RIGHT]: 0,
+});
+
+const isMoving = computed(() => {
+  return Object.values(translation).some((value) => value !== 0);
 });
 
 const fromOffset = props.beadsGrid.resolveBeadOffset(props.selected.from);
@@ -66,14 +87,25 @@ const y = Math.min(fromOffset.y, toOffset.y) - PADDING;
 const width = Math.abs(fromOffset.x - toOffset.x) + BEAD_SIZE + PADDING * 2;
 const height = Math.abs(fromOffset.y - toOffset.y) + BEAD_SIZE + PADDING * 2;
 
-const selectionColor = useSelectionColor();
+const movingX = computed(() => x - translation.left);
+const movingY = computed(() => y - translation.top);
+const movingWidth = computed(() => width + translation.right + translation.left);
+const movingHeight = computed(() => height + translation.bottom + translation.top);
 </script>
 
 <style scoped>
 @layer page {
   .canvas-selection {
-    stroke-width: calc(1 + (1 / v-bind("canvasStore.scale")));
+    stroke-width: calc(var(--selection-size) + (1 / v-bind("canvasStore.scale")));
     will-change: stroke-width;
+  }
+
+  .canvas-selection--original {
+    --selection-size: 1
+  }
+
+  .canvas-selection--moving {
+    --selection-size: 0
   }
 
   .canvas-selection__overlay {
