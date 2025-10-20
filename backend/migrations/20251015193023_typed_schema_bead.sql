67879bc2666c1e35e8c9e461f8410f9e23ae7667
TArch64 <tarasturchenko1551@gmail.com>
2025-10-20 15:13:56 +0300

    fix handling empty objects

--- backend/migrations/20251015193023_typed_schema_bead.sql
+++ backend/migrations/20251015193023_typed_schema_bead.sql
@@ -1,9 +1,13 @@
 UPDATE schemas
-SET beads = (SELECT jsonb_object_agg(
-                      key,
-                      jsonb_build_object(
-                        'kind', 'circle',
-                        'circle', jsonb_build_object('color', value)
-                      )
-                    )
-             FROM jsonb_each_text(schemas.beads));
+SET beads = CASE
+              WHEN beads = '{}'::jsonb THEN '{}'::jsonb
+              ELSE (SELECT jsonb_object_agg(
+                             key,
+                             jsonb_build_object(
+                               'kind', 'circle',
+                               'circle', jsonb_build_object('color', value)
+                             )
+                           )
+                    FROM jsonb_each_text(schemas.beads))
+  END;
+
