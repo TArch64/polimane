31f8507467e9e5b1cf7f1c97dcfff34e1e6455f5
TArch64 <tarasturchenko1551@gmail.com>
2025-10-26 18:27:37 +0200

    fix using invalid column name

--- backend/repository/schemainvitations/list_by_email.go
+++ backend/repository/schemainvitations/list_by_email.go
@@ -11,6 +11,6 @@ import (
 func (c *Client) ListByEmail(ctx context.Context, email string) ([]*model.SchemaInvitation, error) {
 	return gorm.
 		G[*model.SchemaInvitation](c.db).
-		Where("email = ? and expired_at > now()", email).
+		Where("email = ? and expires_at > now()", email).
 		Find(ctx)
 }
