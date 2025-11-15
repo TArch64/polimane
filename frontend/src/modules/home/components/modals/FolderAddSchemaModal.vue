9032f5ce7bad927f0d056a0851c2996a748b3bc4
TArch64 <tarasturchenko1551@gmail.com>
2025-11-15 17:07:15 +0200

    new folder mode

--- frontend/src/modules/home/components/modals/FolderAddSchemaModal.vue
+++ frontend/src/modules/home/components/modals/FolderAddSchemaModal.vue
@@ -8,8 +8,12 @@
       label
       placeholder="Директорія"
       variant="control"
-      v-model="destinationFolderId"
+      v-model="form.folderId"
     >
+      <option :value="NEW_FOLDER_ID">
+        Нова Директорія
+      </option>
+
       <option
         v-for="folder of foldersStore.folders"
         :key="folder.id"
@@ -18,14 +22,24 @@
         {{ folder.name }}
       </option>
     </SelectField>
+
+    <TextField
+      label
+      required
+      placeholder="Назва Директорії"
+      variant="control"
+      class="folder-add-schema-modal__folder-name"
+      v-model="form.folderName"
+      v-if="isNewFolder"
+    />
   </Modal>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, reactive } from 'vue';
 import { Modal, useActiveModal } from '@/components/modal';
 import { useFoldersStore } from '@/modules/home/stores';
-import { SelectField } from '@/components/form';
+import { SelectField, TextField } from '@/components/form';
 import { useAsyncAction } from '@/composables';
 
 const props = defineProps<{
@@ -33,17 +47,32 @@ const props = defineProps<{
   folderId: string | null;
 }>();
 
+const NEW_FOLDER_ID = 'new' as const;
+
 const modal = useActiveModal();
 
 const foldersStore = useFoldersStore();
 
-const destinationFolderId = ref<string | null>(props.folderId);
+const form = reactive({
+  folderId: props.folderId || NEW_FOLDER_ID,
+  folderName: '',
+});
+
+const isNewFolder = computed(() => form.folderId === NEW_FOLDER_ID);
 
 const save = useAsyncAction(async () => {
-  if (props.folderId === destinationFolderId.value) {
+  if (props.folderId === form.folderId) {
     return;
   }
 
   modal.close(null);
 });
 </script>
+
+<style scoped>
+@layer page {
+  .folder-add-schema-modal__folder-name {
+    margin-top: 16px;
+  }
+}
+</style>
