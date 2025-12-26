47eec0c76aa2263eba0daf1a3ccf65c5df48803b
TArch64 <tarasturchenko1551@gmail.com>
2025-12-26 22:58:05 +0200

    fix return not found on get single

--- backend/repository/base/get.go
+++ backend/repository/base/get.go
@@ -9,12 +9,10 @@ import (
 )
 
 func (c *Client[M]) Get(ctx context.Context, scopes ...repository.Scope) (*M, error) {
-	var out M
-	err := c.GetOut(ctx, &out, scopes...)
-	if err != nil {
-		return nil, err
-	}
-	return &out, nil
+	return gorm.
+		G[*M](c.DB).
+		Scopes(scopes...).
+		Take(ctx)
 }
 
 func (c *Client[M]) GetOut(ctx context.Context, out interface{}, scopes ...repository.Scope) error {
