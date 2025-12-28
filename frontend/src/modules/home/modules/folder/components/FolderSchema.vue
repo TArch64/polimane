1880dbeb6b0596800d48b31d44d9cbc9cc149ee7
TArch64 <tarasturchenko1551@gmail.com>
2025-12-28 18:22:16 +0200

    single remove from folder

--- frontend/src/modules/home/modules/folder/components/FolderSchema.vue
+++ frontend/src/modules/home/modules/folder/components/FolderSchema.vue
@@ -14,13 +14,15 @@ import {
 } from '@/modules/home/composables';
 import type { ListSchema } from '@/modules/home/stores';
 import type { MaybeContextMenuAction } from '@/components/contextMenu';
-import { useFolderStore } from '../stores';
+import { FolderRemoveIcon } from '@/components/icon';
+import { useFolderSchemasStore, useFolderStore } from '../stores';
 
 const props = defineProps<{
   schema: ListSchema;
 }>();
 
 const folderStore = useFolderStore();
+const schemasStore = useFolderSchemasStore();
 
 const renameAction = useSchemaMenuCopy(() => props.schema);
 const addToDirectoryAction = useSchemaMenuAddToDirectory(() => props.schema, folderStore.folder.id);
@@ -31,6 +33,13 @@ const deleteAction = useSchemaMenuDelete(() => props.schema);
 const menuActions = computed((): MaybeContextMenuAction[] => [
   renameAction.value,
   addToDirectoryAction.value,
+
+  {
+    title: 'Видалити з Директорії',
+    icon: FolderRemoveIcon,
+    onAction: () => schemasStore.removeSchemaFromFolder(props.schema),
+  },
+
   copyAction.value,
   editAccessAction.value,
   deleteAction.value,
