b962e4dc4dbafb62d2dc960a9b4024ee07a2669c
TArch64 <tarasturchenko1551@gmail.com>
2026-01-02 22:56:42 +0200

    remove extra func

--- backend/repository/delete.go
+++ backend/repository/delete.go
@@ -4,10 +4,8 @@ import (
 	"gorm.io/gorm"
 )
 
-func HardDelete(stmt *gorm.Statement) {
-	stmt.Unscoped = true
-}
-
 func IncludeSoftDeleted(stmt *gorm.Statement) {
 	stmt.Unscoped = true
 }
+
+var HardDelete = IncludeSoftDeleted
