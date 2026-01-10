4396c5c1bd77360e5f3eb04d8bcba481b11b24bb
TArch64 <tarasturchenko1551@gmail.com>
2026-01-10 21:28:12 +0200

    remove unused code

--- backend/repository/joins.go
+++ backend/repository/joins.go
@@ -11,9 +11,3 @@ func Join(query string, args ...interface{}) Scope {
 		})
 	}
 }
-
-func Preload(query string, args ...interface{}) Scope {
-	return func(stmt *gorm.Statement) {
-		stmt.Preload(query, args...)
-	}
-}
--- backend/repository/userschemas/count.go
+++ backend/repository/userschemas/count.go
@@ -3,16 +3,9 @@ package userschemas
 import (
 	"context"
 
-	"gorm.io/gorm"
-
-	"polimane/backend/model"
 	"polimane/backend/repository"
 )
 
 func (c *Client) Count(ctx context.Context, scopes ...repository.Scope) (int64, error) {
 	return c.CountByColumn(ctx, "schema_id", scopes...)
-}
-
-func (c *Client) CountQuery(ctx context.Context, scopes ...repository.Scope) gorm.ChainInterface[model.UserSchema] {
-	return c.CountByColumnQuery(ctx, "schema_id", scopes...)
 }
