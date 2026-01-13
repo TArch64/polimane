00716ae140c251be2eed077c444b8dee6ce7851f
TArch64 <tarasturchenko1551@gmail.com>
2026-01-13 22:16:56 +0200

    add user schema counters to db

---
+++ backend/migrations/00014_add_user_schema_counters.sql
@@ -0,0 +1,27 @@
+-- +goose Up
+ALTER TABLE user_schemas
+  ADD COLUMN IF NOT EXISTS counters jsonb DEFAULT '{}'::jsonb;
+
+
+WITH schema_users_count AS (SELECT schema_id AS id, COUNT(user_id)
+                            FROM user_schemas
+                            GROUP BY schema_id),
+
+     schema_beads_count AS (SELECT id, COUNT(*)
+                            FROM schemas,
+                                 LATERAL JSONB_EACH(beads) AS schema_beads
+                            WHERE schema_beads.value ->> 'kind' != 'ref'
+                            GROUP BY id)
+UPDATE user_schemas
+SET counters = JSON_BUILD_OBJECT(
+  'sharedAccess', schema_users_count.count,
+  'schemaBeads', COALESCE(schema_beads_count.count, 0)
+               )
+FROM schema_users_count
+       LEFT JOIN schema_beads_count ON schema_beads_count.id = schema_users_count.id
+WHERE user_schemas.schema_id = schema_users_count.id;
+
+
+-- +goose Down
+ALTER TABLE user_schemas
+  DROP COLUMN IF EXISTS counters;
--- backend/model/user_schema.go
+++ backend/model/user_schema.go
@@ -1,15 +1,25 @@
 package model
 
+import (
+	t "gorm.io/datatypes"
+)
+
 type UserSchema struct {
 	*Timestamps
 	*SoftDeletable
-	UserID   ID          `gorm:"primaryKey" json:"userId"`
-	SchemaID ID          `gorm:"primaryKey" json:"schemaId"`
-	FolderID *ID         `json:"folderId"`
-	Access   AccessLevel `json:"access"`
+	UserID   ID                          `gorm:"primaryKey" json:"userId"`
+	SchemaID ID                          `gorm:"primaryKey" json:"schemaId"`
+	FolderID *ID                         `json:"folderId"`
+	Access   AccessLevel                 `json:"access"`
+	Counters t.JSONType[*SchemaCounters] `json:"counters" gorm:"default:'{}'::json"`
 
 	// Relations
 	User   *User   `json:"-"`
 	Schema *Schema `json:"-"`
 	Folder *Folder `json:"-"`
+}
+
+type SchemaCounters struct {
+	SchemaBeads  uint16 `json:"schemaBeads"`
+	SharedAccess uint8  `json:"sharedAccess"`
 }
