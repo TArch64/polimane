55d84aad7825d3e105fc98881239e6cb12227278
TArch64 <tarasturchenko1551@gmail.com>
2026-01-17 16:17:34 +0200

    update

--- frontend/src/composables/http/UpdateUserCountersMiddleware.ts
+++ frontend/src/composables/http/UpdateUserCountersMiddleware.ts
@@ -5,6 +5,9 @@ import type { useSessionStore } from '@/stores';
 import { decryptXor, parseIdBytes } from '@/helpers';
 import type { IHttpResponseSuccessInterceptor } from './HttpMiddlewareExecutor';
 
+const USER_COUNTERS_HEADER_BYTES = [88, 45, 85, 115, 101, 114, 45, 67, 111, 117, 110, 116, 101, 114, 115];
+const USER_COUNTERS_HEADER = String.fromCharCode(...USER_COUNTERS_HEADER_BYTES);
+
 export class UpdateUserCountersMiddleware implements IHttpResponseSuccessInterceptor {
   static use(): IHttpResponseSuccessInterceptor {
     return new UpdateUserCountersMiddleware();
@@ -19,10 +22,10 @@ export class UpdateUserCountersMiddleware implements IHttpResponseSuccessInterce
   interceptResponseSuccess(response: Response): MaybePromise<void> {
     if (!this.sessionStore.isLoggedIn) return;
 
-    const input = this.getCountersInput(response);
+    const input = this.getInput(response);
     if (!input) return;
 
-    const counters = this.validateCounters(input);
+    const counters = this.validate(input);
     if (!counters) return;
 
     this.sessionStore.updateUser({
@@ -33,24 +36,24 @@ export class UpdateUserCountersMiddleware implements IHttpResponseSuccessInterce
     });
   }
 
-  private getCountersInput(response: Response): object | null {
-    const encoded = response.headers.get('X-User-Counters');
+  private getInput(response: Response): object | null {
+    const encoded = response.headers.get(USER_COUNTERS_HEADER);
     if (!encoded) return null;
 
     try {
-      return JSON.parse(this.decodeCountersInput(encoded));
+      return JSON.parse(this.decodeInput(encoded));
     } catch {
       return null;
     }
   }
 
-  private decodeCountersInput(encoded: string): string {
+  private decodeInput(encoded: string): string {
     const key = parseIdBytes(this.sessionStore.user.id);
     const encrypted = Uint8Array.from(atob(encoded), (c) => c.charCodeAt(0));
     return new TextDecoder().decode(decryptXor(key, encrypted));
   }
 
-  private validateCounters(input: object): UserCounters | null {
+  private validate(input: object): UserCounters | null {
     const output: UserCounters = {} as UserCounters;
 
     for (const key of UserLimits) {
